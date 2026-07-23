804463790219270a570844e21c1e5e6c6d77146c
Shukrullo <abdusalomovjovox@gmail.com>
2026-07-23 20:18:47 +0500

    server73

--- my-movie/src/components/MovieDetail/MovieDetail.jsx
+++ my-movie/src/components/MovieDetail/MovieDetail.jsx
@@ -1539,8 +1539,8 @@ const MovieDetail = () => {
                 >
                   <svg
                     className="movie-detail-btn-icon"
-                    width="18"
-                    height="18"
+                    width="22"
+                    height="22"
                     viewBox="0 0 24 24"
                     fill="currentColor"
                     aria-hidden="true"
@@ -1555,8 +1555,8 @@ const MovieDetail = () => {
                 >
                   <svg
                     className="movie-detail-btn-icon"
-                    width="18"
-                    height="18"
+                    width="22"
+                    height="22"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
