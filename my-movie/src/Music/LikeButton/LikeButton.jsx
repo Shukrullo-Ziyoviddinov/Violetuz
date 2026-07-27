98546781bc32a4a7c9f47a2fee453414476d9505
Shukrullo <abdusalomovjovox@gmail.com>
2026-07-27 21:34:42 +0500

    klipui55

--- my-movie/src/Music/LikeButton/LikeButton.jsx
+++ my-movie/src/Music/LikeButton/LikeButton.jsx
@@ -207,7 +207,7 @@ const LikeButton = ({
           onClick={wrapClick(handleLikeClick)}
           aria-label="Like"
         >
-          <svg width="12" height="12" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
             <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-2z" />
           </svg>
           <span className="similar-trailer-action-count">{fmt(likeCount)}</span>
@@ -218,7 +218,7 @@ const LikeButton = ({
           onClick={wrapClick(handleDislikeClick)}
           aria-label="Dislike"
         >
-          <svg width="12" height="12" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
             <path d="M15 3H6c-.83 0-1.54.5-1.84 1.22l-3.02 7.05c-.09.23-.14.47-.14.73v2c0 1.1.9 2 2 2h6.31l-.95 4.57-.03.32c0 .41.17.79.44 1.06L9.83 23l6.59-6.59c.36-.36.58-.86.58-1.41V5c0-1.1-.9-2-2-2zm4 0v12h4V3h-4z" />
           </svg>
           <span className="similar-trailer-action-count">{fmt(dislikeCount)}</span>
