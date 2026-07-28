76690dcd9f3ee2f4b52f0a03c2b91e6c6689120d
Shukrullo <abdusalomovjovox@gmail.com>
2026-07-28 17:33:32 +0500

    klipui68

--- my-movie/src/src/Music/Visual/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AudioVisualizerCanvas } from './AudioVisualizerCanvas';
-export { default as ImgButtonVisual } from './imgButtonVisual';
-export { default as CardVisual } from './cardVisual';
-export { default as VisualImgLeftRight } from './VisualImgLeftRight';
--- my-movie/src/src/data/adsData.js
+++
@@ -1,9 +0,0 @@
-
-export const adsData = [
-  {
-    id: 1,
-    videoUrl: '/video/rumovie.mp4',
-    isActive: true
-  }
-
-];
