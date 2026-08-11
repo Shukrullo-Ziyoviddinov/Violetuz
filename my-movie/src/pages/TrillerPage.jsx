187df97818aa97c55034dacc35312e5f7626441c
Shukrullo <abdusalomovjovox@gmail.com>
2026-08-11 19:39:57 +0500

    triller16

--- my-movie/src/pages/TrillerPage.jsx
+++ my-movie/src/pages/TrillerPage.jsx
@@ -7,8 +7,10 @@ const TrillerPage = () => {
   const { id } = useParams();
 
   useEffect(() => {
+    document.documentElement.classList.add('triller-page-lock');
     document.body.classList.add('triller-page-active');
     return () => {
+      document.documentElement.classList.remove('triller-page-lock');
       document.body.classList.remove('triller-page-active');
     };
   }, []);
