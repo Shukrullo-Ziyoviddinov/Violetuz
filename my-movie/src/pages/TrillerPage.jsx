357189285f62ae04c6cd4647a9d24adf3e42d966
Shukrullo <abdusalomovjovox@gmail.com>
2026-08-11 17:48:22 +0500

    triller4

--- my-movie/src/pages/TrillerPage.jsx
+++ my-movie/src/pages/TrillerPage.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Triller from '../components/Triller/Triller';
 import './TrillerPage.css';
 
 const TrillerPage = () => {
   const { id } = useParams();
+
+  useEffect(() => {
+    document.body.classList.add('triller-page-active');
+    return () => {
+      document.body.classList.remove('triller-page-active');
+    };
+  }, []);
 
   return (
     <div className="triller-page">
