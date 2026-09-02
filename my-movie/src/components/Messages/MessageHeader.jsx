9f9b148c07f3519c0be6d719d99731f54ed5b66d
Shukrullo <abdusalomovjovox@gmail.com>
2026-09-02 21:18:22 +0500

    modal icon

--- my-movie/src/components/Messages/MessageHeader.jsx
+++ my-movie/src/components/Messages/MessageHeader.jsx
@@ -3,8 +3,8 @@ import MessageSearch from './MessageSearch';
 import './MessageHeader.css';
 
 const BackIcon = () => (
-  <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" aria-hidden>
-    <path d="M19 12H5M12 19l-7-7 7-7" />
+  <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden>
+    <polyline points="15 18 9 12 15 6" />
   </svg>
 );
 
--- my-movie/src/components/Messages/MessageSearchModal.jsx
+++ my-movie/src/components/Messages/MessageSearchModal.jsx
@@ -6,8 +6,8 @@ import './MessageSearchModal.css';
 const MODAL_ANIMATION_MS = 360;
 
 const BackIcon = () => (
-  <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" aria-hidden>
-    <path d="M19 12H5M12 19l-7-7 7-7" />
+  <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden>
+    <polyline points="15 18 9 12 15 6" />
   </svg>
 );
 
